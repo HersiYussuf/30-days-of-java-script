101f53cad1941fa950db582d305b7b48aff45e1a
Yussuf Hersi <115760894+HersiYussuf@users.noreply.github.com>
2023-08-28 17:11:18 +0300

    Day one done

---
+++ Day_one/datatypes.js
@@ -0,0 +1,26 @@
+let x = "string";
+let y = 8;
+let j
+let v = 9.0909;
+// check the datatypes 
+console.log(typeof(y))
+console.log(typeof(x))
+console.log(typeof(v))
+// Question 6
+let g
+let n
+let o
+let q
+// 7 four variables with assigned values
+let variableOne = "1";
+let varibadlepTwo = "2";
+let variableThree = "3";
+let variaThree = "4"
+// Question 8
+let first_Name = "Yussuf";
+let last_name = "Hersi";
+let maritalStatus = false;
+let country = "Kenya";
+let age = 20 ;
+// print out in single line
+console.log(first_Name,last_name,country, maritalStatus)
---
+++ Day_one/variable.js
@@ -0,0 +1,10 @@
+let x = "string";
+console.log(x)
+let y = 9;
+let n = 90;
+console.log(90<9);
+let k 
+console.log(k)
+let empyValue = null;
+console.log(empyValue)
+
