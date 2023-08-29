7212fb2a1c2830cfcfb5ef2c3a3c68e40ce678a6
Yussuf Hersi <115760894+HersiYussuf@users.noreply.github.com>
2023-08-29 22:30:40 +0300

    day three start

---
+++ Day_Three/Day3.js
@@ -0,0 +1,44 @@
+// Booleans 
+// Boolean values are either true or false.
+// They are mainly used to compare two variables.
+// For example, if we compare two variables using the greater than operator >, it will return a boolean value.
+// Examples
+let isLightOn = true;
+let isRaining = false;
+let hungry = true;
+let isMarried = false;
+let truValue = 4 > 3; // true
+let falseValue = 4 < 3; // false
+// Truthy and Falsy Values
+// In JavaScript, a truthy value is a value that is considered true when encountered in a Boolean context.\
+// All values are truthy unless they are defined as falsy (i.e., except for false, 0, -0, 0n, "", null, 
+// undefined, and NaN).
+// All strings are truthy except empty strings.
+// All numbers are truthy except 0, -0, 0n, NaN, which are falsy.
+// Falsy Values
+  // 0
+  // On
+    // Off
+    // null
+    // undefined
+    // NaN
+    // the boolean false
+    // the empty string ""
+// Undefined
+// If we declare a variable and if we do no assign a value will be undefined
+// If a function is not returning a value then it will be undefined 
+let firstName 
+console.log(firstName)// not defined 
+// Null
+let empty = Null;
+console.log(empty)// -> Null, means no value
+//Operators 
+// Assignment operator
+let firstName2 = "Hersi";
+//Arithmetic Operators 
+   // They are mathematical operators.
+   // Addition(+): a + b;
+   // subtraction(-): a-b;
+   // multiplication(*): a * b;
+   // Division(/): a / b;
+   //modulus(%)
